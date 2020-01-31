26073f6b6e6a69e39e76d6229585dd494ec560be
mgazizova <mgazizova@naumen.ru>
2020-01-31 19:22:11 +0500

    добавила проверку

--- first.py
+++ first.py
@@ -7,7 +7,8 @@ n = int(input())
 def run(word):
     for shift in range(0, 26):
         out_text = encode(word, shift)
-        if out_text in text:
+
+        if out_text is not None and out_text in text:
             print(out_text)
             break
 
@@ -21,9 +22,18 @@ def encode(word, shift):
                 index = (symbs.index(char) + shift) % len(symbs)
                 out += symbs[index]
                 is_decode = True
+                if not isStartWith(out, text):
+                    return None
         if not is_decode:
             out += char
     return out
+
+
+def isStartWith(a, text):
+    for el in text:
+        if el.startswith(a):
+            return True
+    return False
 
 
 for i in range(0, n):
