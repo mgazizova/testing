47793c64e1d0125283f1b9702cf34b1882e76df9
mgazizova <mgazizova@naumen.ru>
2020-01-31 20:23:30 +0500

    вывод строк

--- third.py
+++ third.py
@@ -1,24 +1,29 @@
 # зашифровка
 def encode(numbers, trans):
-    res = list()
+    # res = list()
+    res = str()
     for el in numbers:
         index = trans.index(int(el))
         trans.pop(index)
         trans.insert(0, int(el))
-        res.append(index+1)
+        res += str(index+1) + ' '
+        # res.append(index+1)
     return res
 
 
 # расшифровка
 def decode(numbers, trans):
-    res = list()
+    # res = list()
+    res = str()
     for el in numbers:
-        index = int(el) -1
+        index = int(el)-1
         elem = trans[index]
-        res.append(elem)
+        # res.append(elem)
+        res += str(elem) + ' '
         trans.pop(index)
         trans.insert(0, elem)
     return res
+
 
 while True:
     try:
@@ -28,11 +33,13 @@ while True:
 
         if type == 1:
             # зашифровать
-            encode_list = encode(numbers, transposition)
-            print(' '.join(map(str, encode_list)))
+            # encode_list = encode(numbers, transposition)
+            # print(' '.join(map(str, encode_list)))
+            print(encode(numbers, transposition))
         if type == 2:
             # расшифровать
-            decode_list = decode(numbers, transposition)
-            print(' '.join(map(str, decode_list)))
+            # decode_list = decode(numbers, transposition)
+            # print(' '.join(map(str, decode_list)))
+            print(decode(numbers, transposition))
     except Exception:
         pass
