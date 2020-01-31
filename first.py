e331a0d24db9f1d348ab1f9784f2a8a21f3ea489
mgazizova <mgazizova@naumen.ru>
2020-01-31 17:58:02 +0500

    first commit

---
+++ first.py
@@ -0,0 +1,33 @@
+LANG_SYMBS = ['abcdefghijklmnopqrstuvwxyz', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ']
+
+text = str(input()).split()
+n = int(input())
+
+
+def run(word):
+    for shift in range(0, 26):
+        out_text = encode(word, shift)
+        if out_text in text:
+            print(out_text)
+            break
+
+
+def encode(word, shift):
+    out = ''
+    for char in word:
+        is_decode = False
+        for symbs in LANG_SYMBS:
+            if char in symbs:
+                index = (symbs.index(char) + shift) % len(symbs)
+                out += symbs[index]
+                is_decode = True
+        if not is_decode:
+            out += char
+    return out
+
+
+for i in range(0, n):
+    word = input()
+    run(word)
+
+
